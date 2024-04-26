8ba1220963e85ad3dfa380b7c186cdbda1dd857f
Arsenalchik <117148436+Arsenalchik@users.noreply.github.com>
2024-04-26 04:25:01 +0300

    Update triDraw.m

--- Homework2/triDraw.m
+++ Homework2/triDraw.m
@@ -1,1 +1,14 @@
+function [t,b] = triDraw(p)
 
+DT = delaunayTriangulation(p);
+triplot(DT);
+hold on
+k = convexHull(DT);
+xHull = DT.Points(k,1);
+yHull = DT.Points(k,2);
+plot(xHull,yHull,'r'); 
+hold off
+t = DT.ConnectivityList();
+b = k';
+
+end
